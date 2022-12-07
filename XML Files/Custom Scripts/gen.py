ab60b92b66d367ded5a2cd738b33810e5b5e1bc5
m-kostrzewa <contact@michal-kostrzewa.com>
2022-12-07 01:58:05 +0100

    Add hacking tags.

--- XML Files/Custom Scripts/gen.py
+++ XML Files/Custom Scripts/gen.py
@@ -2,10 +2,10 @@
 from jinja2 import Environment, FileSystemLoader
 
 template_env = Environment(loader=FileSystemLoader("."))
-# TMPL_FNAME = "Tags.xml.tmpl.better"
-# OUT_FNAME = "Tags.xml"
-TMPL_FNAME = "ShieldSharing.xml.tmpl"
-OUT_FNAME = "ShieldSharing.xml"
+TMPL_FNAME = "HackingTags.xml.tmpl"
+OUT_FNAME = "HackingTags.xml"
+# TMPL_FNAME = "ShieldSharing.xml.tmpl"
+# OUT_FNAME = "ShieldSharing.xml"
 
 with open(OUT_FNAME, 'w') as f:
     f.write(template_env.get_template(TMPL_FNAME).render())
