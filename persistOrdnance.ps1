fabe96add2e0fdcb6d071149f01bb6a17bf25d59
m-kostrzewa <contact@michal-kostrzewa.com>
2023-02-12 19:50:14 +0100

    Fix and optimize persistent ordnance.

--- persistOrdnance.ps1
+++ persistOrdnance.ps1
@@ -23,70 +23,91 @@
 
 $scriptLog = "./dat/Missions/MISS_TSN-Command/persistOrdnanceLog.txt"
 
-$StartTime = Get-Date
-echo((Get-Date).GetDateTimeFormats()[40] + "persistOrdnance.ps1 starting") >> $scriptLog
+echo((Get-Date).GetDateTimeFormats()[40] + " starting. Will log to $scriptlog") >> $scriptLog
+echo((Get-Date).GetDateTimeFormats()[40] + " starting. Will log to $scriptlog")
 
-$xmlPath = "./dat/Missions/MISS_TSN-Command/MISS_TSN-Command.xml"
-$logPath = "./dat/Missions/MISS_TSN-Command/MISS_TSN-Command_LOG.txt"
+echo((Get-Date).GetDateTimeFormats()[40] + " checking if persistOrdnance.exe is already running") >> $scriptLog
+echo((Get-Date).GetDateTimeFormats()[40] + " checking if persistOrdnance.exe is already running")
+Get-Process | Where {$_.name -like "persistOrdnance"} | Where {$_.Id -ne "$PID"} | Stop-Process -PassThru | ForEach-Object  {
+    echo((Get-Date).GetDateTimeFormats()[40] + " killed already running persistOrdnance with pid ${existingProcess.Id} ") >> $scriptLog
+    echo((Get-Date).GetDateTimeFormats()[40] + " killed already running persistOrdnance with pid ${existingProcess.Id} ")
+} 
 
-# hardcoded paths for now
-[xml]$xmlDoc = Get-Content $xmlPath
-$log = Get-Content $logPath
+while ($true) {
+
+    echo((Get-Date).GetDateTimeFormats()[40] + " going to sleep for 60 seconds") >> $scriptLog
+    echo((Get-Date).GetDateTimeFormats()[40] + " going to sleep for 60 seconds")
+    Start-Sleep -Seconds 60
+    echo((Get-Date).GetDateTimeFormats()[40] + " waking up") >> $scriptLog
+    echo((Get-Date).GetDateTimeFormats()[40] + " waking up")
+
+    $StartTime = Get-Date
 
 
-# For every player slot
-for ($i = 0; $i -lt 8; $i++) {
+    # hardcoded paths for now
+    $xmlPath = "./dat/Missions/MISS_TSN-Command/MISS_TSN-Command.xml"
+    $logPath = "./dat/Missions/MISS_TSN-Command/MISS_TSN-Command_LOG.txt"
+    [xml]$xmlDoc = Get-Content $xmlPath
+    $log = Get-Content $logPath
 
-    # Parse log line
 
-    # Example log looks like this (see PersistentOrdnance.xml for details).
-# *** mission time: 20 sec ***
-# ship 0: 8.0, 2.0, 6.0, 4.0, 4.0, 5.0, 5.0, 5.0
-# ship 1: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 2: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 3: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 4: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 5: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 6: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 7: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# *** mission time: 30 sec ***
-# ship 0: 6.0, 2.0, 4.0, 4.0, 4.0, 5.0, 5.0, 5.0
-# ship 1: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 2: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 3: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 4: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 5: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 6: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-# ship 7: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # For every player slot
+    for ($i = 0; $i -lt 8; $i++) {
 
-    $shipLogLine = $log | Select-String "ship $i" | Select-Object -Last 1
+        # Parse log line
 
-    if (!$shipLogLine) {
-        echo("no data, return early")  > log.txt
-        return
+        # Example log looks like this (see PersistentOrdnance.xml for details).
+    # *** mission time: 20 sec ***
+    # ship 0: 8.0, 2.0, 6.0, 4.0, 4.0, 5.0, 5.0, 5.0
+    # ship 1: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 2: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 3: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 4: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 5: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 6: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 7: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # *** mission time: 30 sec ***
+    # ship 0: 6.0, 2.0, 4.0, 4.0, 4.0, 5.0, 5.0, 5.0
+    # ship 1: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 2: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 3: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 4: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 5: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 6: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    # ship 7: 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+
+        $shipLogLine = $log | Select-String "ship $i" | Select-Object -Last 1
+
+        if (!$shipLogLine) {
+            echo((Get-Date).GetDateTimeFormats()[40] + " no ordnance data in mission log, skipping this update cycle") >> $scriptLog
+            echo((Get-Date).GetDateTimeFormats()[40] + " no ordnance data in mission log, skipping this update cycle")
+            continue
+        }
+
+        $ordnance = ([string]$shipLogLine).Split(":")[1].Split(",")
+
+        # Find the "ShipX Configuration" node in the XML
+        $shipConfigNode = $xmlDoc.mission_data.event | Where-Object { $_.name -eq "Ship" + ($i+1) + " Configuration" }
+
+        # Substitute ordnance counts.
+        # |nHoming|, |nNuke|, |nMine|, |nEMP|, |nShk|, |nBea|, |nPro|, |nTag|
+        # The reason we cast to int and then to string is because XML wants string, but we remove the decimal point first.
+        ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countHoming"}).value =[string]([int]($ordnance[0]))
+        ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countNuke"}).value =  [string]([int]($ordnance[1]))
+        ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countMine"}).value =  [string]([int]($ordnance[2]))
+        ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countEMP"}).value =   [string]([int]($ordnance[3]))
+        ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countShk"}).value =   [string]([int]($ordnance[4]))
+        ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countBea"}).value =   [string]([int]($ordnance[5]))
+        ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countPro"}).value =   [string]([int]($ordnance[6]))
+        ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countTag"}).value =   [string]([int]($ordnance[7]))
     }
 
-    $ordnance = ([string]$shipLogLine).Split(":")[1].Split(",")
 
-    # Find the "ShipX Configuration" node in the XML
-    $shipConfigNode = $xmlDoc.mission_data.event | Where-Object { $_.name -eq "Ship" + ($i+1) + " Configuration" }
+    # Save method needs absolute path for some reason
+    $absPath = Resolve-Path -Path $xmlPath
+    $xmlDoc.Save($absPath.Path)
 
-    # Substitute ordnance counts.
-    # |nHoming|, |nNuke|, |nMine|, |nEMP|, |nShk|, |nBea|, |nPro|, |nTag|
-    # The reason we cast to int and then to string is because XML wants string, but we remove the decimal point first.
-    ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countHoming"}).value =[string]([int]($ordnance[0]))
-    ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countNuke"}).value =  [string]([int]($ordnance[1]))
-    ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countMine"}).value =  [string]([int]($ordnance[2]))
-    ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countEMP"}).value =   [string]([int]($ordnance[3]))
-    ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countShk"}).value =   [string]([int]($ordnance[4]))
-    ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countBea"}).value =   [string]([int]($ordnance[5]))
-    ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countPro"}).value =   [string]([int]($ordnance[6]))
-    ($shipConfigNode.set_object_property | Where-Object {$_.property -eq "countTag"}).value =   [string]([int]($ordnance[7]))
+    $RunTime = New-TimeSpan -Start $StartTime -End (Get-Date) 
+    echo((Get-Date).GetDateTimeFormats()[40] + " Updated MISS_TSN-Command.xml (" + $absPath.Path + "), took " +  $RunTime.Seconds + " seconds + " + $RunTime.Milliseconds + " miliseconds") >> $scriptLog
+    echo((Get-Date).GetDateTimeFormats()[40] + " Updated MISS_TSN-Command.xml (" + $absPath.Path + "), took " +  $RunTime.Seconds + " seconds + " + $RunTime.Milliseconds + " miliseconds")
 }
-
-# Save method needs absolute path for some reason
-$absPath = Resolve-Path -Path $xmlPath
-$xmlDoc.Save($absPath.Path)
-
-$RunTime = New-TimeSpan -Start $StartTime -End (Get-Date) 
-echo((Get-Date).GetDateTimeFormats()[40] + "- MISS_TSN-Command.xml (" + $absPath.Path + ") updated, took " +  $RunTime.Seconds + " seconds + " + $RunTime.Milliseconds + " miliseconds") >> $scriptLog
